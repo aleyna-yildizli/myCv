707a1c7b8819f54e90a23a0e0a6584be74d6e694
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-02 02:38:38 +0300

    Skills created

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,5 +1,29 @@
 export default function Skills () {
     return (
-        <div>Skills Componenti</div>
+        <div className="flex flex-col gap-4 w-full mb-5">
+        <h2 className="text-4xl font-bold">Skills</h2>
+        <div className="grid grid-cols-3 w-full">
+            <div className="flex flex-col gap-4">
+                <div className="text-indigo-700 text-[20px]" >Java Script</div>
+                <p className="text-[12px] text-gray-900">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat justo enim. Suspendisse ut odio tempus, fermentum ante ac, tempus eros.
+               
+                </p>
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="text-indigo-700 text-[20px]" >React.Js</div>
+                <p className="text-[12px] text-gray-900">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat justo enim. Suspendisse ut odio tempus, fermentum ante ac, tempus eros. 
+               
+                </p>
+            </div>
+            <div className="flex flex-col gap-4">
+                <div className="text-indigo-700 text-[20px]" >Node.Js</div>
+                <p className="text-[12px] text-gray-900">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin volutpat justo enim. Suspendisse ut odio tempus, fermentum ante ac, tempus eros.                
+                </p>
+            </div>
+        </div>
+    </div>
     )
 };

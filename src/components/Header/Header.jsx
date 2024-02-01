aa6aca7b1ae5bbf07556a1e87163d3d342e9aec9
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-01 20:09:12 +0300

    Header css apply

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ export default function Header () {
                 <div className="flex items-center justify-center w-[50px] h-[50px] rounded-[50%] bg-[#EEEBFF]">
                     <span className="text-[25px] rotate-45 text-[#7B61FF] font-semibold">A</span>
                 </div>
-                <ul className="flex items-center gap-16 text-[15px] text-gray-600">
-                    <li className="cursor-pointer px-4 py-2 ">Skills</li>
-                    <li className="cursor-pointer px-4 py-2 ">Projects</li>
+                <ul className="flex items-center gap-16 text-lg leading-7 font-medium text-gray-600">
+                    <li className="cursor-pointer px-4 py-2  text-[#6B7280] ">Skills</li>
+                    <li className="cursor-pointer px-4 py-2 text-[#6B7280] ">Projects</li>
                     <li className="cursor-pointer border border-[#3730A3] text-[#3730A3] px-4 py-2 rounded outline-none ">Hire me</li>
                 </ul>
         </div>

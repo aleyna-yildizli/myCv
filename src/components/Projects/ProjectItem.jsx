6670807ec6d9e6c02949e3c79bea1902bc9e479f
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-03 12:08:08 +0300

    ProjectItem css edited

--- src/components/Projects/ProjectItem.jsx
+++ src/components/Projects/ProjectItem.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom';
 export default function ProjectItem({name, title, description, image}) {
     
   return (
-      <div className='my-10'>
-        <img className=' w-[300px] h-[180px] object-cover' src={image} alt=""/>
+      <div className='my-10 border border-solid border-1 border-[#3730A3]'>
+        <img className=' w-[300px] h-[180px] object-cover ' src={image} alt=""/>
         <h3 className='text-lg font-medium leading-7 text-[#4338CA] my-5'>{title}</h3>
-        <p className='text-[13px] text-gray-800'>{description}</p>
+        <p className='text-[14px]  w-[300px] h-[180px]  text-gray-800'>{description}</p>
         <div className='flex flex-row items-center gap-2 my-5'>
             <Link className='border border-purple-900 p-2 rounded' to="/">react</Link>
             <Link className='border border-purple-900 p-2 rounded' to="/">redux</Link>

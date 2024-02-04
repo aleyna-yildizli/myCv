bef7dcaeb157988a4f905607e3e1c1d4de6326fb
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-05 01:15:34 +0300

    Data created

---
+++ src/data.js
@@ -0,0 +1,90 @@
+[
+    {
+      "en": {
+        "hero": {
+          "name": "Aleyna Yıldızlı",
+          "presentation": "Imaginative Problem Solver | Finding Beauty in Simplicity",
+          "introduction": "Hey there! I'm Aleyna, a comprehensive developer passionate about creating excellent user experiences. With extensive experience in both front-end and back-end development, I always aim to design my projects in a strong, scalable, and innovative manner. As an effective communicator within a team, I'm here to make our projects successful together.", 
+        },
+       
+        "skills":[
+            {
+                "title": "JavaScript",
+                "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            },
+            {
+                "title": "React Js",
+                "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            },
+            {
+                "title": "Node Js",
+                "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            },
+        ],
+
+        "profile": {
+          "aboutMe":  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia. Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+            "content": [
+              {
+                "first": "Birth Date",
+                "second": "06.10.1997"
+              },
+              {
+                "first": "City",
+                "second": "Istanbul"
+              },
+              {
+                "first": "Education",
+                "second": "Ege UNV. Mathematics"
+              },
+              {
+                "first": "Preffered Role",
+                "second": "Frontend, UI"
+              }
+            ]
+          },
+        "projects": [
+            {
+              "title": "Workintech",
+              "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+              "tags": [
+                "react",
+                "redux",
+                "axios",
+              ],
+              "github": "Github",
+              "View": "View Site"
+            },
+            {
+                "title": "Random Jokes",
+                "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+                "tags": [
+                  "react",
+                  "redux",
+                  "axios",
+                ],
+                "github": "Github",
+                "View": "View Site"
+              },
+              {
+                "title": "Journey",
+                "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+                "tags": [
+                  "react",
+                  "redux",
+                  "axios",
+                ],
+                "github": "Github",
+                "View": "View Site"
+              },
+          ],
+        "footer": [
+            {
+                "text": "Let’s work together on your next product.",
+                "mail": "aleynayildizli@hmail.com",
+
+            },
+        ]
+        } }]
+
+     

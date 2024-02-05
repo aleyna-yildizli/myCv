98e361c927ec925bb62f4154fa4277e3b1f0fdd3
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-05 18:56:45 +0300

    LanguageProvider to main apply

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 import { myStore } from "./store/store";
+import { LanguageProvider } from './context/LanguageContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 <Provider store={myStore}>
+  <LanguageProvider>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </LanguageProvider>
 </Provider>
 );

5e28d63e8f657ee9352ff4629c3b5d03e001869e
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-05 18:04:13 +0300

    Added data.js

--- src/data.js
+++ src/data.js
@@ -1,209 +1,212 @@
 [
-    {
-      "en": {
-        "hero": {
-          "name": "Aleyna Yıldızlı",
-          "presentation": "Imaginative Problem Solver | Finding Beauty in Simplicity",
-          "introduction": "Hey there! I'm Aleyna, a comprehensive developer passionate about creating excellent user experiences. With extensive experience in both front-end and back-end development, I always aim to design my projects in a strong, scalable, and innovative manner. As an effective communicator within a team, I'm here to make our projects successful together.",
-          "image": "images/IMG_3048.jpg"
+  {
+    "en": {
+      "hero": {
+        "name": "Aleyna Yıldızlı",
+        "presentation": "Imaginative Problem Solver | Finding Beauty in Simplicity",
+        "introduction": "Hey there! I'm Aleyna, a comprehensive developer passionate about creating excellent user experiences. With extensive experience in both front-end and back-end development, I always aim to design my projects in a strong, scalable, and innovative manner. As an effective communicator within a team, I'm here to make our projects successful together.",
+        "image": "images/IMG_3048.jpg"
+      },
+      "skills": [
+        {
+          "title": "JavaScript",
+          "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
-        "skills": [
-          {
-            "title": "JavaScript",
-            "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          },
-          {
-            "title": "React Js",
-            "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          },
-          {
-            "title": "Node Js",
-            "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          }
-        ],
-        "profile": {
-          "aboutMe": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia. Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
-          "content": [
-            {
-              "first": "Birth Date",
-              "second": "06.10.1997"
-            },
-            {
-              "first": "City",
-              "second": "Istanbul"
-            },
-            {
-              "first": "Education",
-              "second": "Ege UNV. Mathematics"
-            },
-            {
-              "first": "Preferred Role",
-              "second": "Frontend, UI"
-            }
-          ]
+        {
+          "title": "React Js",
+          "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
-        "projects": [
+        {
+          "title": "Node Js",
+          "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+      ],
+      "profile": {
+        "aboutMe": {
+          "first": "Hello! I'm Aleyna Yıldızlı, passionate about staying updated on the latest trends and technologies in the ever-evolving field of frontend development. My commitment to excellence, teamwork, and continuous improvement makes me excited about contributing to your projects.",
+          "second": "I am currently seeking new opportunities to apply my skills and bring value to a dynamic team. Please feel free to reach out if you have any questions or if you're interested in discussing potential collaborations. I'm looking forward to hearing from you!"
+        },
+        "content": [
           {
-            "title": "Workintech",
-            "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
-            "tags": [
-              "react",
-              "redux",
-              "axios"
-            ],
-            "github": "Github",
-            "View": "View Site"
+            "first": "Birth Date",
+            "second": "06.10.1997"
           },
           {
-            "title": "Random Jokes",
-            "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
-            "tags": [
-              "react",
-              "redux",
-              "axios"
-            ],
-            "github": "Github",
-            "View": "View Site"
+            "first": "City",
+            "second": "Istanbul"
           },
           {
-            "title": "Journey",
-            "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
-            "tags": [
-              "react",
-              "redux",
-              "axios"
-            ],
-            "github": "Github",
-            "View": "View Site"
-          }
-        ],
-        "footer": [
+            "first": "Education",
+            "second": "Ege UNV. Mathematics"
+          },
           {
-            "text": {
-              "line1": "Let's work together on",
-              "line2": "your next product.",
-            },
-            "mail": "aleynayildizli@gmail.com",
-            "links": [
-              {
-                "text": "Personal Blog",
-                "link": "https://vercel.com/aleynas-projects-f12a5b3d"
-              },
-              {
-                "text": "Github",
-                "link": "https://github.com/aleyna-yildizli"
-              },
-              {
-                "text": "Linkedin",
-                "link": "https://www.linkedin.com/in/aleynayldizli/"
-              }
-            ]
+            "first": "Preferred Role",
+            "second": "Frontend, UI"
           }
         ]
       },
-      "tr": {
-        "hero": {
-          "name": "Aleyna Yıldızlı",
-          "presentation": "Hayalperest Problem Çözücü | Basitlikte Güzelliği Bulan",
-          "introduction": "Merhaba! Ben Aleyna, mükemmel kullanıcı deneyimleri yaratmaya tutkulu kapsamlı bir geliştiriciyim. Hem ön yüz hem de arka yüz geliştirme konusunda geniş deneyime sahip olarak projelerimi her zaman güçlü, ölçeklenebilir ve yenilikçi bir şekilde tasarlamayı amaçlıyorum. Bir ekip içinde etkili bir iletişimci olarak, projelerimizi birlikte başarılı kılmak için buradayım.",
-          "image": "images/IMG_3048.jpg"
+      "projects": [
+        {
+          "title": "Workintech",
+          "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          "tags": [
+            "react",
+            "redux",
+            "axios"
+          ],
+          "github": "Github",
+          "View": "View Site"
         },
-        "skills": [
-          {
-            "title": "JavaScript",
-            "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        {
+          "title": "Random Jokes",
+          "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          "tags": [
+            "react",
+            "redux",
+            "axios"
+          ],
+          "github": "Github",
+          "View": "View Site"
+        },
+        {
+          "title": "Journey",
+          "info": "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          "tags": [
+            "react",
+            "redux",
+            "axios"
+          ],
+          "github": "Github",
+          "View": "View Site"
+        }
+      ],
+      "footer": [
+        {
+          "text": {
+            "first": "Let's work together on",
+            "second": "your next product."
           },
-          {
-            "title": "React Js",
-            "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          },
-          {
-            "title": "Node Js",
-            "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          }
-        ],
-        "profile": {
-          "aboutMe": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia. Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
-          "content": [
+          "mail": "aleynayildizli@gmail.com",
+          "links": [
             {
-              "first": "Doğum Tarihi",
-              "second": "06.10.1997"
+              "text": "Personal Blog",
+              "link": "https://vercel.com/aleynas-projects-f12a5b3d"
             },
             {
-              "first": "İkamet Şehri",
-              "second": "İzmir"
+              "text": "Github",
+              "link": "https://github.com/aleyna-yildizli"
             },
             {
-              "first": "Eğitim Durumu",
-              "second": "Ege Üniversitesi, Matematik"
-            },
-            {
-              "first": "Tercih Edilen Rol",
-              "second": "Frontend, UI"
+              "text": "Linkedin",
+              "link": "https://www.linkedin.com/in/aleynayldizli/"
             }
           ]
+        }
+      ]
+    },
+    "tr": {
+      "hero": {
+        "name": "Aleyna Yıldızlı",
+        "presentation": "Hayalperest Problem Çözücü | Basitlikte Güzelliği Bulan",
+        "introduction": "Merhaba! Ben Aleyna, mükemmel kullanıcı deneyimleri yaratmaya tutkulu kapsamlı bir geliştiriciyim. Hem ön yüz hem de arka yüz geliştirme konusunda geniş deneyime sahip olarak projelerimi her zaman güçlü, ölçeklenebilir ve yenilikçi bir şekilde tasarlamayı amaçlıyorum. Bir ekip içinde etkili bir iletişimci olarak, projelerimizi birlikte başarılı kılmak için buradayım.",
+        "image": "images/IMG_3048.jpg"
+      },
+      "skills": [
+        {
+          "title": "JavaScript",
+          "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         },
-        "projects": [
+        {
+          "title": "React Js",
+          "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+          "title": "Node Js",
+          "description": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+      ],
+      "profile": {
+        "aboutMe": {
+          "first": "Merhaba! Ben Aleyna Yıldızlı, Frontend geliştirmenin hızla değişen dünyasındaki son trendleri ve teknolojileri takip etmeye büyük bir tutkuyla bağlıyım. Mükemmeliyete olan bağlılığım, takım çalışmasına uyum sağlama yeteneğim ve sürekli gelişime olan açık tutumum, projelerinize katkıda bulunma konusunda heyecanlı olduğumu gösteriyor.",
+          "second": "Şu anda becerilerimi uygulayabileceğim ve dinamik bir ekibe değer katabileceğim yeni fırsatlar arıyorum. Herhangi bir sorunuz varsa veya potansiyel işbirliklerini tartışmak istiyorsanız, lütfen benimle iletişime geçmekten çekinmeyin. Size haber almak için sabırsızlanıyorum!"
+        },
+        "content": [
           {
-            "title": "Workintech",
-            "info": 
-            "Basit, özelleştirilebilir, minimal kurulum gerektiren bir çerez eklentisi. Kullanıcılarınıza hangi çerezleri kabul edip reddedeceklerini seçme imkanı tanır. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturuldu ve bir NPM paketi olarak kullanılabilir. Ayrıca, git deposu üzerinden kod ve temaların her türlü özelleştirmesine olanak tanır.",
-            "tags": [
-              "react",
-              "redux",
-              "axios"
-            ],
-            "github": "Github",
-            "View": "View Site"
+            "first": "Doğum Tarihi",
+            "second": "06.10.1997"
           },
           {
-            "title": "Random Jokes",
-            "info": 
-            "Basit, özelleştirilebilir, minimal kurulum gerektiren bir çerez eklentisi. Kullanıcılarınıza hangi çerezleri kabul edip reddedeceklerini seçme imkanı tanır. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturuldu ve bir NPM paketi olarak kullanılabilir. Ayrıca, git deposu üzerinden kod ve temaların her türlü özelleştirmesine olanak tanır.",
-            "tags": [
-              "react",
-              "redux",
-              "axios"
-            ],
-            "github": "Github",
-            "View": "View Site"
+            "first": "İkamet Şehri",
+            "second": "İzmir"
           },
           {
-            "title": "Journey",
-            "info": 
-            "Basit, özelleştirilebilir, minimal kurulum gerektiren bir çerez eklentisi. Kullanıcılarınıza hangi çerezleri kabul edip reddedeceklerini seçme imkanı tanır. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturuldu ve bir NPM paketi olarak kullanılabilir. Ayrıca, git deposu üzerinden kod ve temaların her türlü özelleştirmesine olanak tanır.",
-            "tags": [
-              "react",
-              "redux",
-              "axios"
-            ],
-            "github": "Github",
-            "View": "View Site"
-          }
-        ],
-        "footer": [
+            "first": "Eğitim Durumu",
+            "second": "Ege Üniversitesi, Matematik"
+          },
           {
-            "text": {
-              "line1": "Gelecek projenizde",
-              "line2": "birlikte çalışmak isterim",
-            },
-            "mail": "aleynayildizli@gmail.com",
-            "links": [
-              {
-                "text": "Kişisel Blog",
-                "link": "https://vercel.com/aleynas-projects-f12a5b3d"
-              },
-              {
-                "text": "Github",
-                "link": "https://github.com/aleyna-yildizli"
-              },
-              {
-                "text": "Linkedin",
-                "link": "https://www.linkedin.com/in/aleynayldizli/"
-              }
-            ]
+            "first": "Tercih Edilen Rol",
+            "second": "Frontend, UI"
           }
         ]
-      }
+      },
+      "projects": [
+        {
+          "title": "Workintech",
+          "info": "Basit, özelleştirilebilir, minimal kurulum gerektiren bir çerez eklentisi. Kullanıcılarınıza hangi çerezleri kabul edip reddedeceklerini seçme imkanı tanır. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturuldu ve bir NPM paketi olarak kullanılabilir. Ayrıca, git deposu üzerinden kod ve temaların her türlü özelleştirmesine olanak tanır.",
+          "tags": [
+            "react",
+            "redux",
+            "axios"
+          ],
+          "github": "Github",
+          "View": "View Site"
+        },
+        {
+          "title": "Random Jokes",
+          "info": "Basit, özelleştirilebilir, minimal kurulum gerektiren bir çerez eklentisi. Kullanıcılarınıza hangi çerezleri kabul edip reddedeceklerini seçme imkanı tanır. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturuldu ve bir NPM paketi olarak kullanılabilir. Ayrıca, git deposu üzerinden kod ve temaların her türlü özelleştirmesine olanak tanır.",
+          "tags": [
+            "react",
+            "redux",
+            "axios"
+          ],
+          "github": "Github",
+          "View": "View Site"
+        },
+        {
+          "title": "Journey",
+          "info": "Basit, özelleştirilebilir, minimal kurulum gerektiren bir çerez eklentisi. Kullanıcılarınıza hangi çerezleri kabul edip reddedeceklerini seçme imkanı tanır. Bu, vanilla JS, SCSS ve Parcel Bundler ile oluşturuldu ve bir NPM paketi olarak kullanılabilir. Ayrıca, git deposu üzerinden kod ve temaların her türlü özelleştirmesine olanak tanır.",
+          "tags": [
+            "react",
+            "redux",
+            "axios"
+          ],
+          "github": "Github",
+          "View": "View Site"
+        }
+      ],
+      "footer": [
+        {
+          "text": {
+            "first": "Gelecek projenizde",
+            "second": "birlikte çalışmak isterim"
+          },
+          "mail": "aleynayildizli@gmail.com",
+          "links": [
+            {
+              "text": "Kişisel Blog",
+              "link": "https://vercel.com/aleynas-projects-f12a5b3d"
+            },
+            {
+              "text": "Github",
+              "link": "https://github.com/aleyna-yildizli"
+            },
+            {
+              "text": "Linkedin",
+              "link": "https://www.linkedin.com/in/aleynayldizli/"
+            }
+          ]
+        }
+      ]
     }
-  ]
+  }
+]

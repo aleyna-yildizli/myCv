515b032387202aaa9aaacdac4b97a8c9c0d683ea
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-07 02:56:58 +0300

    Thunk deleted but error setup

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,9 @@
 import { applyMiddleware, legacy_createStore as createStore } from 'redux';
 import logger from "redux-logger";
-import thunk from "redux-thunk";
 import reducer from './reducer.js';
 
 
 
-export const myStore = createStore(reducer, applyMiddleware(thunk, logger));
+export const myStore = createStore(reducer, applyMiddleware(logger));
 
 

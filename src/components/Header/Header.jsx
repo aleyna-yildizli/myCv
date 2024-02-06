02b1bdf6f4bc3ea3c8f1343cdb521c9faad723b8
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-07 02:09:11 +0300

    Last commit canselled this commit purget

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header () {
     return (
         <div className="flex items-center justify-between w-full h-[62px]">
                 <div className="flex items-center justify-center w-[50px] h-[50px] rounded-[50%] bg-[#EEEBFF] dark:bg-[#4731D3]">
-                    <span className="text-[25px] rotate-45 text-[#7B61FF] font-semibold dark:text-[#8F88FF]">A</span>
+                    <span className="text-[25px]  rotate-45 text-[#7B61FF] font-semibold dark:text-[#8F88FF]">A</span>
                 </div>
                 <nav>
-                <ul className="flex items-center gap-16 text-lg leading-7 font-medium text-gray-600">
-                    <li className="cursor-pointer px-4 py-2  text-[#6B7280] "><Link to='/'>Skills</Link></li>
-                    <li className="cursor-pointer px-4 py-2 text-[#6B7280] ">Projects</li>
+                <ul className="flex items-center xl:gap-16 lg:gap-4 md:gap-2 gap-1 xl:text-lg lg:text-base md:text-sm text-xs leading-7 font-medium text-gray-600">
+                    <li className="cursor-pointer xl:px-4 xl:py-2 px-2 py-1  text-[#6B7280] "><Link to='/'>Skills</Link></li>
+                    <li className="cursor-pointer xl:px-4 xl:py-2 px-2 py-1 text-[#6B7280] ">Projects</li>
                     <li className="cursor-pointer border border-[#3730A3] text-[#3730A3] bg-white px-4 py-2 rounded outline-none ">Hire me</li>
                 </ul>
                 </nav>

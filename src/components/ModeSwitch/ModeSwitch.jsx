5b37ea51d37b2833399181684ba779da9ad2e06a
aleyna-yildizli <aleynayildizli@gmail.com>
2024-02-07 00:25:38 +0300

    Skills responsive and heart toggle add :)

--- src/components/ModeSwitch/ModeSwitch.jsx
+++ src/components/ModeSwitch/ModeSwitch.jsx
@@ -1,4 +1,7 @@
 import { IoToggleSharp } from "react-icons/io5";
+import Toggle from 'react-toggle';
+import "react-toggle/style.css"
+import { FaHeart } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import { theme, languageTr } from '../../store/action';
 import useLocalStorage from "../../hook/useLocalStorage";
@@ -29,7 +32,19 @@ export default function ModeSwitch ({data}) {
         <div className="media_500 flex items-center justify-between xl:pt-[23px] lg:pt-[15px] md:pt-[10px] pt-[5px]">
             <div></div>
               <div className="flex items-center xl:gap-4 lg:gap-2 gap-1 xl:text-[15px] lg:text-[12px] text-[10px] letter-spacing: 0.1em font-semibold xl:mb-[24px] lg:mb-[16px]  mb-[10px]">
-                <div className="text-[#777777] dark:text-gray-300 flex items-center"> <IoToggleSharp onClick={toggleTheme} className="w-[60px] h-[30px]"/>DARK MODE</div>
+                <div className="text-[#777777] dark:text-[#D9D9D9] flex items-center"> 
+                <Toggle
+                        defaultChecked={isTheme === "dark"}
+                        icons={{
+                            checked: <FaHeart />,
+                            unchecked: null,
+                          }}
+                        onChange={toggleTheme}
+                    />
+                 <span className="mx-3">
+                {isTheme === "dark" ? "LIGHT MODE" : "DARK MODE"}
+                </span>
+                </div>
                     |
                     <button onClick={handleChangeLanguage}>
                         <span className="text-[#4731D3] cursor-pointer dark:text-[#BAB2E7s]">

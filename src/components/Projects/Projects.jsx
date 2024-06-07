44ad6829a9d7fbc7c98e3842dedc7fe42e2abc3a
aleyna-yildizli <aleynayildizli@gmail.com>
2024-06-07 16:30:25 +0300

    Refactor: project info and links

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -17,6 +17,21 @@ export default function ProjectItem({ data }) {
     }
   }, [data, lang]);
 
+  const links = [
+    {
+      github: "https://github.com/aleyna-yildizli/pizza-adv",
+      view: "https://pizza-adv.vercel.app/",
+    },
+    {
+      github: "https://github.com/aleyna-yildizli/hellokedy-pro",
+      view: "https://hellokedy-pro.vercel.app/",
+    },
+    {
+      github: "https://github.com/aleyna-yildizli/aleyna-ecommerce",
+      view: "https://piggybank.aleynayildizli.com/",
+    },
+  ];
+
   return (
     <div
       id="projects"
@@ -25,7 +40,7 @@ export default function ProjectItem({ data }) {
       <h2 className="xl:text-[48px] lg:text-[40px] md:text-[32px] text-[24px]  font-bold dark:text-[#AEBCCF] xl:mb-5 lg:mb-3 md:mb-2 mb-1">
         Projects
       </h2>
-      <div className="flex flex-wrap justify-between gap-4">
+      <div className="flex flex-wrap justify-between gap-20 sm:gap-2">
         {projectData.map((project, index) => (
           <div
             key={index}
@@ -46,31 +61,39 @@ export default function ProjectItem({ data }) {
                 {project.info}
               </p>
             )}
-            <div className="flex flex-row items-center gap-8 xl:my-5 lg:my-3 md:my-2 my-1 xl:text-base md:text-sm text-xs">
+            <div className="flex flex-row flex-wrap items-center gap-1 xl:my-5 lg:my-3 md:my-2 my-1 xl:text-base md:text-sm text-xs">
               {project.tags.map((tag, tagIndex) => (
-                <Link
+                <button
                   key={tagIndex}
                   className="p-1.5 border border-[#3730A3] rounded text-[#3730A3] dark:text-[#8F88FF] dark:border-[#383838]"
                   to="/"
                 >
                   {tag}
-                </Link>
+                </button>
               ))}
             </div>
             <div className="flex justify-between">
-              {project.github && (
-                <Link to={"/"}>
+              {links[index].github && (
+                <a
+                  href={links[index].github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="font-medium flex-1 underline dark:text-[#E1E1FF] text-[#3730A3] xl:text-base md:text-sm text-xs">
                     {project.github}
                   </button>
-                </Link>
+                </a>
               )}
-              {project.view && (
-                <Link to={"/"}>
+              {links[index].view && (
+                <a
+                  href={links[index].view}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="font-medium flex underline dark:text-[#E1E1FF] text-[#3730A3] xl:text-base md:text-sm text-xs">
                     {project.view}
                   </button>
-                </Link>
+                </a>
               )}
             </div>
           </div>
